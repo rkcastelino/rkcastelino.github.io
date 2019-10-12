e907d147706575ba87b43d992152c0cbdc64f63a
Rohan Castelino <rkcastelino@berkeley.edu>
2019-10-12 00:16:02 -0700

    testing with example

--- processedResults.js
+++ processedResults.js
@@ -1,23 +1,23 @@
-function graph_generator(){
-  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-  // append the svg object to the body of the page
-  var svg = d3.select("#my_dataviz")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-  //Read the data
-  d3.csv("out.csv",
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
 
   // When reading the csv, I must format variables:
   function(d){
-    return { date : d3.timeParse("%m%d/%Y")(d.date), value : d.value }
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
   },
 
   // Now I can use this dataset:
@@ -50,4 +50,3 @@ function graph_generator(){
         )
 
 })
-}
